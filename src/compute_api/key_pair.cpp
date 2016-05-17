f1458f1ecb24881b73e67bc4b59a39e38ec71b27
Gowtham-Baskar <gowtham10896@gmail.com>
2016-05-17 11:49:16 +0530

    requestify.cpp and key_pair.cpp added

---
+++ src/compute_api/key_pair.cpp
@@ -0,0 +1,60 @@
+#include "utils.cpp"
+#include "requestify.cpp"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <map>
+
+using namespace std;
+
+string describe_instances(http_var &info)
+{
+	map <string, string> params;
+	params['Action'] = "DescribeKeyPairs";
+	params['Version'] = info.version;
+	
+	return make_request(info, params);	// make_request function in 'requestify.cpp'
+}
+
+
+string create_key_pair(http_var &info, string key_name)
+{
+	map <string, string> params;
+	params['Action'] = "CreateKeyPair";
+	params['Version'] = info.version;
+	
+	if(key_name.length() == 0)
+	{	
+		cout<<"Key-Name needed"<<endl;
+		//// TODO :
+		// Raise exeception and return
+	}
+	else
+	{
+		params['KeyName'] = key_name;
+	}
+
+	return make_request(info, params);	// make_request function in 'requestify.cpp'
+}
+
+string delete_key_pairs(http_var &info, string key_name)
+{
+	map <string, string> params;
+	params['Action'] = "DeleteKeyPair";
+	params['Version'] = info.version;
+
+	if(key_name.length() == 0)
+	{	
+		cout<<"KeyName needed"<<endl;
+		//// TODO :
+		// Raise exeception and return
+	}
+	else
+	{
+		params['KeyName'] = key_name;
+	}
+
+	return make_request(info, params);	// make_request function in 'requestify.cpp'
+}
+
+string import_key_pair()
