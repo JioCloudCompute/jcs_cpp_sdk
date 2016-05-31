1baa9a02d803ad0c6a0303d1f31cf0bed495ef07
Suman <sumanswar12@gmail.com>
2016-05-31 13:13:10 +0530

    Added Comments

--- src/compute.h
+++ src/compute.h
@@ -22,57 +22,137 @@ using namespace volume;
 using namespace utils;
 using namespace config;
 
+/// \namespace JIOCOMPUTE
 namespace JIOCOMPUTE{
 	class compute
 	{
 		utils::http_var info;
 
 		public:
-			
+		///Constructor
+		/**
+			Initializes the compute endpoint url, Type of HTTP request, Version
+		*/
 		compute();
-
+		///Method to get info of the requested images 
+		/**
+			\param req : Type describe_images_request object 
+			\return describe_images_response object pointer
+		*/
 		describe_images_response *describe_images(const describe_images_request &req);
-
+		///Method to get info of the requested or all instances
+		/**
+			\param req : Type describe_instances_request object
+			\return describe_instances_response object poinrer
+		*/
 		describe_instances_response *describe_instances(const describe_instances_request &req);
-		
+		///Method to get info of types of instances
+		/**
+			\param req : Type describe_instances_request 
+			\return describe_instances_response object pointer
+		*/
 		describe_instance_types_response *describe_instance_types(const describe_instance_types_request &req);
-		
+		///Method to start requested instances
+		/**
+			\param req : Type start_instances_request
+			\return start_instances_response object pointer
+		*/
 		start_instances_response *start_instances(const start_instances_request &req);
-
+		///Method to stop requested instances
+		/**
+			\param req : Type stop_instances_request 
+			\return stop_instances_response object pointer
+		*/
 		stop_instances_response *stop_instances(const stop_instances_request &req);
-
+		///Method to reboot requested instances
+		/**
+			\param req : Type reboot_instances_request
+			\return reboot_instance_response object pointer
+		*/
 		reboot_instances_response *reboot_instances(const reboot_instances_request &req);
-
+		///Method to terminate requested instances
+		/**
+			\param req : Type terminate_instances_request 
+			\return terminate_instances_response objedt pointer
+		*/
 		terminate_instances_response *terminate_instances(const terminate_instances_request &req);
-
+		///Method to create instances
+		/**
+			\param req : Type run_instances_request
+			\return run_instances_response object pointer
+		*/
 		run_instances_response *run_instances(const run_instances_request &req);
-
+		///Method to get info of all key pairs
 		describe_key_pairs_response *describe_key_pairs();
-
+		///Method to create a Key Pair
+		/**
+			\param req : Type create_key_pair_request
+			\return create_key_pair_response object pointer
+		*/
 		create_key_pair_response *create_key_pair(const create_key_pair_request &req);
-
+		///Method to delete a Key Pair
+		/**
+			\param req : Type delete_key_pair_request
+			\return delete_key_pair_response object pointer
+		*/
 		delete_key_pair_response *delete_key_pair(const delete_key_pair_request &req);
-
+		///Method to Import Key Pair
+		/**
+			\param req : Type import_key_pair_request
+			\return import_key_pair_response object POinter
+		*/
 		import_key_pair_response *import_key_pair(const import_key_pair_request &req);
-
+		///Method to get info of all / requested Snapshots
+		/**
+			\param req : Type describe_snapshots_request 
+			\return describe_snapshots_response object Pointer
+		*/
 		describe_snapshots_response *describe_snapshots(const describe_snapshots_request &req);
-
+		///Methode to create a Snapshot
+		/**
+			\param req : Type create_snapshot_request
+			\return create_snapshot_response object pointer
+		*/	
 		create_snapshot_response *create_snapshot(const create_snapshot_request &req);
-
+		///Method to delete a snapshot
+		/**
+			\param req : Type delete_snapshot_request
+			\return delete_snapshot_response object pointer
+		*/
 		delete_snapshot_response *delete_snapshot(const delete_snapshot_request &req);
-
+		///Method to describe all/requested volumes
+		/**
+			\param req : describe_volumes_request
+			\return describe_volumes_response objec pointer
+		*/	
 		describe_volumes_response *describe_volumes(const describe_volumes_request &req);
-
+		///Method to create Volumes
+		/**
+			\param req : Type create_volume_request
+			\return create_volume_response object pointer
+		*/
 		create_volume_response *create_volume(const create_volume_request &req);
-
+		///Method to delete volume 
+		/**
+			\param req : Type delete_volume_request
+			\return delete_volume_response object pointer
+		*/
 		delete_volume_response *delete_volume(const delete_volume_request &req);
-
+		///Method to attach volume
+		/**
+		*/
 		attach_volume_response *attach_volume(const attach_volume_request &req);
-
+		///Method to detach volume
+		/**
+		*/
 		detach_volume_response *detach_volume(const detach_volume_request &req);
-
+		///Method to get info of the delete on termination flag of volume
+		/**
+		*/
 		show_delete_on_termination_flag_response *show_delete_on_termination_flag(const show_delete_on_termination_flag_request &req);
-
+		///Method to update delet on termination flag of a respictive volume
+		/**
+		*/
 		update_delete_on_termination_flag_response *update_delete_on_termination_flag(const update_delete_on_termination_flag_request &req);
 	
 
--- src/config.cpp
+++ src/config.cpp
@@ -4,23 +4,22 @@
 #include <fstream>
 // TODO :read from configuration files or hard COde >??
 namespace config{
+	// Setting up the various endpoints of the API.
 	std::map<std::string,std::string> endpoints;
-
-
 	void set_up_endpoints()
 	{	
 
-		endpoints["vpc"]="https://vpc.ind-west-1.jiocloudservices.com";
-		endpoints["iam"] = "https://iam.ind-west-1.jiocloudservices.com";
-		endpoints["rds"] = "https://rds.ind-west-1.jiocloudservices.com";
-		endpoints["dss"]= "https://dss.ind-west-1.jiocloudservices.com";
-		endpoints["compute"] = "https://compute.ind-west-1.staging.jiocloudservices.com";
+		endpoints["vpc"]="https://vpc.ind-west-1.jiocloudservices.com"; // Virtual Private Cloud Endpoint
+		endpoints["iam"] = "https://iam.ind-west-1.jiocloudservices.com"; // Authentication Endpoint
+		endpoints["rds"] = "https://rds.ind-west-1.jiocloudservices.com"; // Data Storage Enpoint
+		endpoints["dss"]= "https://dss.ind-west-1.jiocloudservices.com";// Durable Data Storage Endpoint
+		endpoints["compute"] = "https://compute.ind-west-1.staging.jiocloudservices.com"; // Compute Endpoint
 	}
 
 	std::string get_service_url(std::string service_name)
 	{	
 		set_up_endpoints();
-		return endpoints[service_name];
+		return endpoints[service_name]; // return the respective service's endpoint url 
 	}
 
 
@@ -28,11 +27,11 @@ namespace config{
 	{
 		private:
 			//endpoints already
-			bool secure;
-			bool debug;
+			bool secure;	// Whether the HTTP connection is secure
+			bool debug;		// Debug mode
 
 			// read this from config file 
-			std::string access_key;
+			std::string access_key;	
 			std::string secret_key;
 
 		public:
@@ -41,13 +40,14 @@ namespace config{
 				//read this from configuration file
 				secure = true;
 				debug = false;
-				// reading keys from file
+
+				// reading keys from file 
 				std::ifstream infile("config.txt");
 				std::string line;
-				getline(infile,line);
+				getline(infile,line); // First Line is the access Key
 				access_key = line;
-				getline(infile,line);
-				secret_key = line;
+				getline(infile,line); // Second Line is the secret Key
+				secret_key = line; 
 				infile.close();
 
 				//set up endpoints
@@ -57,20 +57,21 @@ namespace config{
 				std::cout<<"Unknown Credentials:Access key or secret key not set";
 				}
 			}
-
-			std::string get_access_key()
+			// Getter for access Key
+			std::string get_access_key() const
 			{
 				return access_key;
 			}
-			std::string get_secret_key()
+			//Getter for secret Key
+			std::string get_secret_key() const
 			{
 				return secret_key;
 			}
-			bool check_secure()
+			bool check_secure() const
 			{
 				return secure;
 			}
-			bool check_debug()
+			bool check_debug() const
 			{
 				return debug;
 			}
